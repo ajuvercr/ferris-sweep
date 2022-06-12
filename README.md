d8342cdd12aff8d6741fbbaeee5bc23d2331f677
ajuvercr <arthur.vercruysse@outlook.com>
2022-06-12 19:01:28 +0200

    add joystick support

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ferris-sweep
+
+## Install
+
+
+```
+git submodule init --recursive
+
+cd qmk_firmware/keyboards/ferris/keymaps/
+ln -s ../../../../ git
+
+cd ../../..
+```
+
+then everything is ready just flash the keyboards.
+
+```
+make ferris/sweep:git:dfu-split-right
+make ferris/sweep:git:dfu-split-left
+```
---
+++ rules.mk
@@ -0,0 +1,31 @@
+
+MOUSEKEY_ENABLE        = no
+THUMBSTICK_ENABLE  	   = yes
+# SPLIT_TRANSPORT        = custom
+POINTING_DEVICE_DRIVER = custom
+BOOTLOADER = atmel-dfu
+
+ifeq ($(strip $(THUMBSTICK_ENABLE)), yes)
+    POINTING_DEVICE_ENABLE = yes
+    OPT_DEFS += -DTHUMBSTICK_ENABLE
+	SRC += analog.c
+	SRC += thumbstick.c
+endif
+
+ifeq ($(strip $(SPLIT_TRANSPORT)), custom)
+	OPT_DEFS += -DCUSTOM_TRANSPORT
+	QUANTUM_SRC += transport_custom.c
+	# Functions added via QUANTUM_LIB_SRC are only included in the final binary if they're called.
+	# Unused functions are pruned away, which is why we can add multiple drivers here without bloat.
+	ifeq ($(PLATFORM),AVR)
+		QUANTUM_LIB_SRC += i2c_master.c \
+							i2c_slave.c
+	endif
+
+	SERIAL_DRIVER ?= bitbang
+	ifeq ($(strip $(SERIAL_DRIVER)), bitbang)
+		QUANTUM_LIB_SRC += serial.c
+	else
+		QUANTUM_LIB_SRC += serial_$(strip $(SERIAL_DRIVER)).c
+	endif
+endif
